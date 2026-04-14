a286c46ee0fe92b231e9d5d4020bc3bded433642
oneagencybly-eng <oneagencybly@gmail.com>
2026-04-14 21:37:25 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index.tsx";
 
 const App = () => {
-  return <Index />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+      </Routes>
+    </HashRouter>
+  );
 };
 
 export default App;
