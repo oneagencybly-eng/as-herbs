0195268c9600c40f49875fbf5cf538ad1b7bbbc8
oneagencybly-eng <oneagencybly@gmail.com>
2026-04-14 21:41:56 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index.tsx";
-import NotFound from "./pages/NotFound.tsx";
-import AdminLogin from "./pages/AdminLogin.tsx";
-import AdminDashboard from "./pages/AdminDashboard.tsx";
 import ThankYou from "./pages/ThankYou.tsx";
-import TelecallerLogin from "./pages/TelecallerLogin.tsx";
-import TelecallerDashboard from "./pages/TelecallerDashboard.tsx";
 
 const App = () => {
   return (
@@ -13,11 +8,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/thank-you" element={<ThankYou />} />
-        <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        <Route path="/telecaller" element={<TelecallerLogin />} />
-        <Route path="/telecaller/dashboard" element={<TelecallerDashboard />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </HashRouter>
   );
