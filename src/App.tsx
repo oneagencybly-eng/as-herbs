6dd3e42c04faba93a79051db812c9e6cf76143b4
oneagencybly-eng <oneagencybly@gmail.com>
2026-04-14 21:45:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index.tsx";
-import ThankYou from "./pages/ThankYou.tsx";
+
+const ThankYouTest = () => {
+  return (
+    <div style={{ padding: "40px", fontSize: "24px" }}>
+      Thank You route is working
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/thank-you" element={<ThankYou />} />
+        <Route path="/thank-you" element={<ThankYouTest />} />
       </Routes>
     </HashRouter>
   );
