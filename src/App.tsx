40db70b6249cf55fa0a6cdf4533fc0719ede297c
oneagencybly-eng <oneagencybly@gmail.com>
2026-04-14 19:24:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Index from "./pages/Index.tsx";
-import NotFound from "./pages/NotFound.tsx";
-import AdminLogin from "./pages/AdminLogin.tsx";
-import AdminDashboard from "./pages/AdminDashboard.tsx";
-import ThankYou from "./pages/ThankYou.tsx";
-import TelecallerLogin from "./pages/TelecallerLogin.tsx";
-import TelecallerDashboard from "./pages/TelecallerDashboard.tsx";
-
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/thank-you" element={<ThankYou />} />
-      <Route path="/admin" element={<AdminLogin />} />
-      <Route path="/admin/dashboard" element={<AdminDashboard />} />
-      <Route path="/telecaller" element={<TelecallerLogin />} />
-      <Route path="/telecaller/dashboard" element={<TelecallerDashboard />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
-);
+const App = () => {
+  return (
+    <div style={{ padding: "40px", fontSize: "24px" }}>
+      AS Herbs Home
+    </div>
+  );
+};
 
 export default App;
