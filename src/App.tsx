df0e32c03af799af55fb23e7f75a4e5f43560b84
oneagencybly-eng <oneagencybly@gmail.com>
2026-04-14 21:18:05 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import AdminLogin from "./pages/AdminLogin.tsx";
+import AdminDashboard from "./pages/AdminDashboard.tsx";
 
 const App = () => {
   return <Index />;
