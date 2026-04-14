661437ca4f1d99990a6f55c491600cbdf2e65a99
oneagencybly-eng <oneagencybly@gmail.com>
2026-04-14 21:04:54 +0530

    test Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,32 +1,7 @@
-import { useState } from "react";
-import TopBanner from "@/components/TopBanner";
-import Navbar from "@/components/Navbar";
-import HeroSection from "@/components/HeroSection";
-import BenefitsSection from "@/components/BenefitsSection";
-import BeforeAfterSection from "@/components/BeforeAfterSection";
-import ReviewsSection from "@/components/ReviewsSection";
-import OrderCTA from "@/components/OrderCTA";
-import OrderFormDialog from "@/components/OrderFormDialog";
-import MetaPixel from "@/components/MetaPixel";
-import Footer from "@/components/Footer";
-import { useVisitorTracking } from "@/hooks/useVisitorTracking";
-
 const Index = () => {
-  const [orderOpen, setOrderOpen] = useState(false);
-  useVisitorTracking();
-
   return (
-    <div className="min-h-screen">
-      <MetaPixel />
-      <TopBanner />
-      <Navbar />
-      <HeroSection onOrderClick={() => setOrderOpen(true)} />
-      <BenefitsSection />
-      <BeforeAfterSection />
-      <ReviewsSection />
-      <OrderCTA onOrderClick={() => setOrderOpen(true)} />
-      <OrderFormDialog open={orderOpen} onOpenChange={setOrderOpen} />
-      <Footer />
+    <div style={{ padding: "40px", fontSize: "24px" }}>
+      Index page is working
     </div>
   );
 };
